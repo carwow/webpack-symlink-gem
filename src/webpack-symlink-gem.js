14d94d8e6d011dc99b962d94aeeb1c83287e971e
Anson Kelly <ansonkelly@gmail.com>
2018-02-16 20:30:03 +0000

    .

--- src/webpack-symlink-gem.js
+++ src/webpack-symlink-gem.js
@@ -28,52 +28,52 @@ function clean(rootPath, gem) {
 function linkGem(rootPath, config) {
   console.log('LINKING GEM: ', config.name);
 
-  let gemPath = execSync(gemShowCmd(config.name)).toString().trim();
-  console.log('gemPath: ', gemPath);
-  let version = path.parse(gemPath).base.match(/-(\d+\.\d+\.\d+)/i)[1];
+  // let gemPath = execSync(gemShowCmd(config.name)).toString().trim();
+  // console.log('gemPath: ', gemPath);
+  // let version = path.parse(gemPath).base.match(/-(\d+\.\d+\.\d+)/i)[1];
 
-  var cmd = "git clone -b 'v" + version + "' --single-branch --depth 1 " + config.gitrepo + " ./vendor/gems/" + config.name;
-  console.log('Command: ', cmd);
-  execSync(cmd);
+  // var cmd = "git clone -b 'v" + version + "' --single-branch --depth 1 " + config.gitrepo + " ./vendor/gems/" + config.name;
+  // console.log('Command: ', cmd);
+  // execSync(cmd);
 
-  // console.log('CWD: ', execSync('pwd').toString().trim());
-  // // console.log('ls ~/vendor/bundle/ruby/2.4.0/gems/ ', execSync('ls ./vendor/bundle/ruby/2.4.0/gems/').toString().trim());
-  // let gemPathRoot;
-  // const gem = config.name;
-  // const gemPath = config.gemPath || "";
+  console.log('CWD: ', execSync('pwd').toString().trim());
+  // console.log('ls ~/vendor/bundle/ruby/2.4.0/gems/ ', execSync('ls ./vendor/bundle/ruby/2.4.0/gems/').toString().trim());
+  let gemPathRoot;
+  const gem = config.name;
+  const gemPath = config.gemPath || "";
 
-  // clean(rootPath, gem);
+  clean(rootPath, gem);
 
-  // try {
-  //   gemPathRoot = execSync(gemShowCmd(gem)).toString().trim();
-  //   console.log('bundle show ', gem, ': ', gemPathRoot);
-  // } catch (err) {
-  //   console.log('ERROR getting gemPathRoot!');
-  //   // compilation.errors.push(makeError(err.message));
-  //   return;
+  try {
+    gemPathRoot = execSync(gemShowCmd(gem)).toString().trim();
+    console.log('bundle show ', gem, ': ', gemPathRoot);
+  } catch (err) {
+    console.log('ERROR getting gemPathRoot!');
+    // compilation.errors.push(makeError(err.message));
+    return;
+  }
+
+  console.log('ls gemPathRoot ', execSync('ls -lah ' + gemPathRoot).toString().trim());
+  console.log('ls gemPathRoot ', execSync('ls -lah ' + gemPathRoot + '/').toString().trim());
+
+  if (!fs.existsSync(gemPathRoot)) {
+    console.log('ERROR - gemPathRoot does not exist! ', gemPathRoot);
+  }
+
+  const fullGemPath = path.join(gemPathRoot, gemPath);
+  console.log('fullGemPath: ', fullGemPath);
+  console.log('COPYING DIR OVER ', fullGemPath, ' to ', localPath(rootPath, gem));
+  copydir.sync(fullGemPath, localPath(rootPath, gem));
+  // if (!fs.existsSync(fullGemPath)) {
+  //   // compilation.errors.push(makeError("Path for gem was invalid: " + fullGemPath));
+  //   // return;
+  //   console.log("WARNING: Path for gem was invalid, webpack may not compile: " + fullGemPath);
   // }
 
-  // console.log('ls gemPathRoot ', execSync('ls -lah ' + gemPathRoot).toString().trim());
-  // console.log('ls gemPathRoot ', execSync('ls -lah ' + gemPathRoot + '/').toString().trim());
+  // fs.symlinkSync(fullGemPath, localPath(rootPath, gem));
 
-  // if (!fs.existsSync(gemPathRoot)) {
-  //   console.log('ERROR - gemPathRoot does not exist! ', gemPathRoot);
-  // }
-
-  // const fullGemPath = path.join(gemPathRoot, gemPath);
-  // console.log('fullGemPath: ', fullGemPath);
-  // console.log('COPYING DIR OVER ', fullGemPath, ' to ', localPath(rootPath, gem));
-  // copydir.sync(fullGemPath, localPath(rootPath, gem));
-  // // if (!fs.existsSync(fullGemPath)) {
-  // //   // compilation.errors.push(makeError("Path for gem was invalid: " + fullGemPath));
-  // //   // return;
-  // //   console.log("WARNING: Path for gem was invalid, webpack may not compile: " + fullGemPath);
-  // // }
-
-  // // fs.symlinkSync(fullGemPath, localPath(rootPath, gem));
-
-  // console.log('ls -lah ~/vendor/gems/ ', execSync('ls -lah ./vendor/gems/').toString().trim());
-  // console.log('ls -lah ~/vendor/gems/carwow_theme/ ', execSync('ls -lah ./vendor/gems/carwow_theme/').toString().trim());
+  console.log('ls -lah ~/vendor/gems/ ', execSync('ls -lah ./vendor/gems/').toString().trim());
+  console.log('ls -lah ~/vendor/gems/carwow_theme/ ', execSync('ls -lah ./vendor/gems/carwow_theme/').toString().trim());
 }
 
 class WebpackSymlinkGem {
